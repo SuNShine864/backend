1ec93b60a252e63cf28272b7326cd5b9f0039723
SuNShine864 <100672568+SuNShine864@users.noreply.github.com>
2026-04-15 00:34:29 +0530

    Fix typo in cookie options for secure flag

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -163,7 +163,7 @@ const logoutUser=asyncHandler(async(req,res)=>{
         )
         const options={
             httpOnly:true,
-            ecure:true
+            secure:true
         } 
         return res
         .status(200)
